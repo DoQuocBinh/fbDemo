052d6bc929b93858678b47b54cb87a2cd5b695fc
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-12-19 11:18:14 +0700

    update token support

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp();
+exports.userSignUp = functions.auth.user().onCreate((user)=>{
+    if (user.email &&
+        user.email == 'binhdq@fpt.edu.vn' &&
+        user.emailVerified) {
+        const customClaims = {
+            admin: true,
+            accessLevel: 9
+        };
+        return admin.auth().setCustomUserClaims(user.uid, customClaims)
+        .catch(error => {
+            console.log(error);
+          });
+    }else
+    {
+        console.warn('not logged1');
+        console.warn(user.email);
+        console.warn(user.emailVerified);
+    }
+})
