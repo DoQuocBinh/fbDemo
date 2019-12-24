75a54e4132494b553159155842809acf54427a16
Binh Do Quoc <binhdq@fpt.edu.vn>
2019-12-24 10:44:21 +0700

    install ExpressJS and Handlerbars

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
+const express = require('express');
+const engines = require('consolidate');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -7,7 +10,47 @@ const admin = require('firebase-admin');
 //  response.send("Hello from Firebase!");
 // });
 
+//To install handlebars: 
+//npm i handlebars consolidate --save
+
 admin.initializeApp();
+
+const app = express();
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
+exports.app = functions.https.onRequest(app);
+
+app.get('/',(request,response) =>{
+	const ref = admin.database().ref('facts');
+	var facts =[];
+	ref.once('value').then(snap=> {
+		snap.forEach(function (child){
+			var fact = {
+				key : child.key,
+				text : child.child('/fact').val()
+			}
+			var l = facts.push(fact);
+		})
+		response.render('index',{facts});
+	}
+	).catch(error =>{
+		response.redirect("/");
+	})
+});
+
+exports.bigben = functions.https.onRequest((req,res)=>{
+    const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
+    res.status(200).send(`<!doctype html>
+    <head>
+      <title>Time</title>
+    </head>
+    <body>
+      ${'BONG '.repeat(hours)}
+    </body>
+  </html>`);
+
+})
 exports.userSignUp = functions.auth.user().onCreate((user)=>{
     if (user.email &&
         user.email == 'binhdq@fpt.edu.vn' &&
